74b5fd2ebe7aa6ea018aedb85d8caf7e64beac29
Shinichi Ishimura <ishimura@treasure-data.com>
2018-09-27 16:34:56 +0900

    Support specifying line delimiter character.

--- embulk-core/src/main/java/org/embulk/spi/util/LineDecoder.java
+++ embulk-core/src/main/java/org/embulk/spi/util/LineDecoder.java
@@ -5,11 +5,14 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+
+import com.google.common.base.Optional;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
 import org.embulk.config.Task;
@@ -26,10 +29,14 @@ public class LineDecoder implements AutoCloseable, Iterable<String> {
         @Config("newline")
         @ConfigDefault("\"CRLF\"")
         public Newline getNewline();
+
+        @Config("line_delimiter")
+        @ConfigDefault("null")
+        public Optional<Newline> getLineDelimiter();
     }
 
     private final FileInputInputStream inputStream;
-    private final BufferedReader reader;
+    private final LineReader reader;
     private final Charset charset;
 
     public LineDecoder(FileInput in, DecoderTask task) {
@@ -39,7 +46,9 @@ public class LineDecoder implements AutoCloseable, Iterable<String> {
                 .onMalformedInput(CodingErrorAction.REPLACE)  // TODO configurable?
                 .onUnmappableCharacter(CodingErrorAction.REPLACE);  // TODO configurable?
         this.inputStream = new FileInputInputStream(in);
-        this.reader = new BufferedReader(new InputStreamReader(inputStream, decoder));
+        this.reader = new LineReader(
+                new InputStreamReader(inputStream, decoder), task.getLineDelimiter().orNull(), 256
+        );
     }
 
     public boolean nextFile() {
@@ -133,6 +142,72 @@ public class LineDecoder implements AutoCloseable, Iterable<String> {
         @Override
         public void remove() {
             throw new UnsupportedOperationException();
+        }
+    }
+
+    /**
+     * A {@link BufferedReader} that can specify line delimiter character from any one of CR, LF and CRLF.
+     * If not specified, follow the original {@link BufferedReader}'s behavior.
+     */
+    private static class LineReader extends BufferedReader {
+        private final Newline lineDelimiter;   // should create another enum?
+        private final char[] buffer;
+        private int offset = 0;
+        private int charsRead = 0;
+
+        LineReader(Reader reader, Newline lineDelimiter, int bufferSize) {
+            super(reader);
+            this.lineDelimiter = lineDelimiter;
+            this.buffer = new char[bufferSize];
+        }
+
+        @Override
+        public String readLine() throws IOException {
+            if (lineDelimiter == null) {
+                return super.readLine();
+            }
+
+            StringBuilder line = null;
+            bufferLoop:
+            while (offset > 0 || (charsRead = this.read(buffer)) != -1) {
+                if (line == null) {
+                    line = new StringBuilder(); // Initialize line's buffer for the first loop
+                }
+                for (int i = offset; i < charsRead; i++) {
+                    char c = buffer[i];
+                    boolean isEOL = false;
+                    switch (lineDelimiter) {
+                        case CR:
+                            if (c == '\r') {
+                                isEOL = true;
+                            }
+                            break;
+                        case LF:
+                            if (c == '\n') {
+                                isEOL = true;
+                            }
+                            break;
+                        case CRLF:
+                            if ((line.length() > 0 && line.charAt(line.length() - 1) == '\r') && c == '\n') {
+                                // Delete unnecessary CR
+                                line.deleteCharAt(line.length() - 1);
+                                isEOL = true;
+                            }
+                            break;
+                    }
+                    offset++;
+                    if (isEOL) {
+                        break bufferLoop;
+                    }
+                    line.append(c);
+                }
+                offset = 0;
+            }
+
+            if (line != null) {
+                return line.toString();
+            }
+            return null;
         }
     }
 }
